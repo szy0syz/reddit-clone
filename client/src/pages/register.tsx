5a7472a4f7dd289cf4af3f81434c060283d10df4
szy0syz <szy0syz@gmail.com>
2020-12-15 13:40:24 +0800

    Initial 初始化客户端

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Register Page
+        </h1>
+
+      </main>
+
+
+    </div>
+  )
+}
