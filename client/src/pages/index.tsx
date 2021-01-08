51b23511c8dffa459cffdb7e270ec5d75f0e06f2
Jerry <szy0syz@gmail.com>
2021-01-08 18:21:50 +0800

    Update

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,10 +2,17 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="pt-12">
       <Head>
         <title>readit: the front page of the internet</title>
       </Head>
+      <div >
+        <div className="container flex pr-4">
+          <div className="w-160">
+            w-160
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
