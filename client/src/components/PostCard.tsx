b128295a24bee5e1299486d8a505518f921b5400
Jerry <szy0syz@gmail.com>
2021-01-25 14:52:11 +0800

    Update #17 Post Page

--- client/src/components/PostCard.tsx
+++ client/src/components/PostCard.tsx
@@ -1,73 +1,71 @@
-import dayjs from "dayjs";
-import React from "react";
 import Link from "next/link";
-import relativeTime from "dayjs/plugin/relativeTime";
 import Axios from "axios";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import classNames from "classnames";
+
 import { Post } from "../types";
-import { useRouter } from "next/router";
-import { useAuthState } from "../context/auth";
+import ActionButton from "./ActionButton";
 
 dayjs.extend(relativeTime);
 
 interface PostCardProps {
   post: Post;
-  revalidate?: Function;
 }
 
-const ActionButton = ({ children }) => {
-  return (
-    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-      {children}
-    </div>
-  );
-};
-
-export default function PostCard({ post, revalidate }: PostCardProps) {
-  const router = useRouter();
-  const { authenticated } = useAuthState();
-
-  const vote = async (value) => {
-    if (!authenticated) router.push("/login");
-
-    if (value === post.userVote) value = 0;
-
+export default function PostCard({
+  post: {
+    identifier,
+    slug,
+    title,
+    body,
+    subName,
+    createdAt,
+    voteScore,
+    userVote,
+    commentCount,
+    url,
+    username,
+  },
+}: PostCardProps) {
+  const vote = async (value: number) => {
     try {
-      await Axios.post("/misc/vote", {
+      const res = await Axios.post("/misc/vote", {
+        identifier,
+        slug,
         value,
-        slug: post.slug,
-        identifier: post.identifier,
       });
 
-      if (revalidate) revalidate();
-    } catch (error) {
-      console.log(error);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
     }
   };
+
   return (
-    <div className="flex mb-4 bg-white rounded" id={post.identifier}>
-      {/* vote section */}
-      <div className="w-10 py-3 text-center bg-gray-200 rounded-lg">
+    <div key={identifier} className="flex mb-4 bg-white rounded">
+      {/* Vote section */}
+      <div className="w-10 py-3 text-center bg-gray-200 rounded-l">
         {/* Upvote */}
         <div
-          onClick={() => vote(1)}
           className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+          onClick={() => vote(1)}
         >
           <i
             className={classNames("icon-arrow-up", {
-              "text-red-500": post.userVote === 1,
+              "text-red-500": userVote === 1,
             })}
           ></i>
         </div>
-        <p>{post.voteScore}</p>
+        <p className="text-xs font-bold">{voteScore}</p>
         {/* Downvote */}
         <div
+          className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
           onClick={() => vote(-1)}
-          className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-500"
         >
           <i
             className={classNames("icon-arrow-down", {
-              "text-blue-600": post.userVote === -1,
+              "text-blue-600": userVote === -1,
             })}
           ></i>
         </div>
@@ -75,42 +73,41 @@ export default function PostCard({ post, revalidate }: PostCardProps) {
       {/* Post data section */}
       <div className="w-full p-2">
         <div className="flex items-center">
-          <Link href={`/r/${post.subName}`}>
+          <Link href={`/r/${subName}`}>
             <img
               src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
               className="w-6 h-6 mr-1 rounded-full cursor-pointer"
             />
           </Link>
-          <Link href={`/r/${post.subName}`}>
+          <Link href={`/r/${subName}`}>
             <a className="text-xs font-bold cursor-pointer hover:underline">
-              /r/{post.subName}
+              /r/{subName}
             </a>
           </Link>
           <p className="text-xs text-gray-500">
             <span className="mx-1">•</span>
             Posted by
-            <Link href={`{/u/${post.username}}`}>
-              <a href="" className="mx-1 hover:underline">
-                {post.username}
-              </a>
+            <Link href={`/u/${username}`}>
+              <a className="mx-1 hover:underline">/u/{username}</a>
             </Link>
-            <Link href={post.url}>
+            <Link href={url}>
               <a className="mx-1 hover:underline">
-                {dayjs(post.createdAt).fromNow()}
+                {dayjs(createdAt).fromNow()}
               </a>
             </Link>
           </p>
         </div>
-        <Link href={post.url}>
-          <a className="my-1 text-lg font-medium">{post.title}</a>
+        <Link href={url}>
+          <a className="my-1 text-lg font-medium">{title}</a>
         </Link>
-        {post.body && <p className="my-1 text-sm">{post.body}</p>}
+        {body && <p className="my-1 text-sm">{body}</p>}
+
         <div className="flex">
-          <Link href={post.url}>
+          <Link href={url}>
             <a>
               <ActionButton>
                 <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                <span className="font-bold">{post.commentCount} Comments</span>
+                <span className="font-bold">{commentCount} Comments</span>
               </ActionButton>
             </a>
           </Link>
--- client/src/pages/r/[sub]/[identifier]/[slug].tsx
+++ client/src/pages/r/[sub]/[identifier]/[slug].tsx
@@ -1,20 +1,55 @@
-import useSWR from "swr";
-import Link from "next/link";
-import Head from "next/head";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { Post } from "../../../../types";
-import Sidebar from "../../../../components/Sidebar";
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import Image from 'next/image'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import classNames from 'classnames'
+
+import { Post } from '../../../../types'
+import Sidebar from '../../../../components/Sidebar'
+import Axios from 'axios'
+import { useAuthState } from '../../../../context/auth'
+import ActionButton from '../../../../components/ActionButton'
+
+dayjs.extend(relativeTime)
 
 export default function PostPage() {
-  const router = useRouter();
-  const { identifier, sub, slug } = router.query;
+  // Local state
+
+  // Global state
+  const { authenticated } = useAuthState()
+
+  // Utils
+  const router = useRouter()
+  const { identifier, sub, slug } = router.query
 
   const { data: post, error } = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null
-  );
+  )
 
-  if (error) router.push("/");
+  if (error) router.push('/')
+
+  const vote = async (value: number) => {
+    // If not logged in go to login
+    if (!authenticated) router.push('/login')
+
+    // If vote is the same reset vote
+    if (value === post.userVote) value = 0
+
+    try {
+      const res = await Axios.post('/misc/vote', {
+        identifier,
+        slug,
+        value,
+      })
+
+      console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   return (
     <>
@@ -22,15 +57,15 @@ export default function PostPage() {
         <title>{post?.title}</title>
       </Head>
       <Link href={`/r/${sub}`}>
-        <a href="">
+        <a>
           <div className="flex items-center w-full h-20 p-8 bg-blue-500">
             <div className="container flex">
               {post && (
                 <div className="w-8 h-8 mr-2 overflow-hidden rounded-full">
                   <Image
                     src={post.sub.imageUrl}
-                    width={(8 * 16) / 2}
-                    height={(8 * 16) / 2}
+                    height={(8 * 16) / 4}
+                    width={(8 * 16) / 4}
                   />
                 </div>
               )}
@@ -43,12 +78,83 @@ export default function PostPage() {
         {/* Post */}
         <div className="w-160">
           <div className="bg-white rounded">
-            {post && <div className="flex">{/* Vote section */}</div>}
+            {post && (
+              <div className="flex">
+                {/* Vote section */}
+                <div className="w-10 py-3 text-center rounded-l">
+                  {/* Upvote */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-red-500"
+                    onClick={() => vote(1)}
+                  >
+                    <i
+                      className={classNames('icon-arrow-up', {
+                        'text-red-500': post.userVote === 1,
+                      })}
+                    ></i>
+                  </div>
+                  <p className="text-xs font-bold">{post.voteScore}</p>
+                  {/* Downvote */}
+                  <div
+                    className="w-6 mx-auto text-gray-400 rounded cursor-pointer hover:bg-gray-300 hover:text-blue-600"
+                    onClick={() => vote(-1)}
+                  >
+                    <i
+                      className={classNames('icon-arrow-down', {
+                        'text-blue-600': post.userVote === -1,
+                      })}
+                    ></i>
+                  </div>
+                </div>
+                <div className="p-2">
+                  <div className="flex items-center">
+                    <p className="text-xs text-gray-500">
+                      Posted by
+                      <Link href={`/u/${post.username}`}>
+                        <a className="mx-1 hover:underline">
+                          /u/{post.username}
+                        </a>
+                      </Link>
+                      <Link href={post.url}>
+                        <a className="mx-1 hover:underline">
+                          {dayjs(post.createdAt).fromNow()}
+                        </a>
+                      </Link>
+                    </p>
+                  </div>
+                  {/* Post title */}
+                  <h1 className="my-1 text-xl font-medium">{post.title}</h1>
+                  {/* Post body */}
+                  <p className="my-3 text-sm">{post.body}</p>
+                  {/* Actions */}
+                  <div className="flex">
+                    <Link href={post.url}>
+                      <a>
+                        <ActionButton>
+                          <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                          <span className="font-bold">
+                            {post.commentCount} Comments
+                          </span>
+                        </ActionButton>
+                      </a>
+                    </Link>
+                    <ActionButton>
+                      <i className="mr-1 fas fa-share fa-xs"></i>
+                      <span className="font-bold">Share</span>
+                    </ActionButton>
+                    <ActionButton>
+                      <i className="mr-1 fas fa-bookmark fa-xs"></i>
+                      <span className="font-bold">Save</span>
+                    </ActionButton>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         {/* Sidebar */}
         {post && <Sidebar sub={post.sub} />}
       </div>
     </>
-  );
+  )
 }
